673be6e64694ca6386bee10a152f99796bacdb77
LCKxD <46042487+LCKxD@users.noreply.github.com>
2018-12-24 01:39:01 -0500

    Add files via upload

---
+++ LCKBWI.py
@@ -0,0 +1,26 @@
+import socket 
+#Why are you looking at my code bro -_- ..... xD By: L  C  K
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+
+print("Welcome to the L  C  K basic web info tool!")
+
+
+print(" ")
+server = raw_input('Enter a website: ')
+port= 80
+print(" ")
+server_ip = socket.gethostbyname(server) 
+print( server + " ip is "+server_ip)
+print(" ")
+request= "GET / HTTP/1.1\nHost: "+server+"\n\n" 
+
+s.connect((server, port))
+s.send(request.encode())
+resutl= s.recv(4096)
+
+print(resutl)
+
+while (len(resutl) > 0):
+	print(resutl)               
+	result= s.recv(4096)
